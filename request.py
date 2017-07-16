4dc81c7bbddb89e5d7e6849512dbee9d029d0c34
rajan rana <rajan.rana@gmail.com>
2017-07-15 23:57:17 -0400

    get more data use use two fun. and get self information

--- request.py
+++ request.py
@@ -6,6 +6,27 @@ BASE_URL= 'https://api.instagram.com/v1/'
 def self_info():
     url =(BASE_URL+ 'users/self/?access_token=%s') %(ACCESS_TOKEN)
     r=requests.get(url).json()
-    return r['data']
-
+    if r['meta']['code'] == 200:
+        if len(r['data']):
+            print "your profile pic is %s :" %(r['data']['profile_picture'])
+        else:
+            print '..'
+    else:
+        return "you have entered wrong value"
 print self_info()
+
+def user_info():
+    url =(BASE_URL+ 'users/self/?access_token=%s') %(ACCESS_TOKEN)
+    r=requests.get(url).json()
+    if r['meta']['code'] == 200:
+        if len(r['data']):
+            print 'Username: %s' % (r['data']['username'])
+            print 'No. of followers: %s' % (r['data']['counts']['followed_by'])
+            print 'No. of people you are following: %s' % (r['data']['counts']['follows'])
+            print 'No. of posts: %s' % (r['data']['counts']['media'])
+        else:
+            print 'User does not exist!'
+    else:
+        print 'Status code other than 200 received!'
+
+print user_info()
